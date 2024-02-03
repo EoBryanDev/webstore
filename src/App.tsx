f57ac41eed658cd4fbbce9f82a7e304cdefa60da
Bryan <mauriciobryan1998@gmail.com>
2024-02-03 14:07:30 -0300

    Installing eslint and editing app page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {
+  SafeAreaView,
+  Text,
+  // View
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Testandoo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
