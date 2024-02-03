15650d359ede31032c27bcca056465a6fa5c7ae1
Bryan <mauriciobryan1998@gmail.com>
2024-02-03 14:28:07 -0300

    Installing styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import {
   SafeAreaView,
-  Text,
+  StyleSheet,
   // View
 } from 'react-native';
+import styled from 'styled-components/native';
 
+const NewText = styled.Text`
+  font-size: 32px;
+  color: white;
+`;
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Testandoo</Text>
+      <NewText style={styles.container}>Teste</NewText>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    padding: 4,
+    margin: 10,
+  },
+});
 
 export default App;
