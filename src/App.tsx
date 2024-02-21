02ed26825f30d580e09274aa512de50763ec0529
Bryan <mauriciobryan1998@gmail.com>
2024-02-21 20:33:55 -0300

    Creating Folder Tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  // View
-} from 'react-native';
-import styled from 'styled-components/native';
+import { SafeAreaView } from 'react-native';
 
-const NewText = styled.Text`
-  font-size: 32px;
-  color: white;
-`;
+import Login from './modules/login';
+
 const App = () => {
   return (
     <SafeAreaView>
-      <NewText style={styles.container}>Teste</NewText>
+      <Login />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-    padding: 4,
-    margin: 10,
-  },
-});
 
 export default App;
