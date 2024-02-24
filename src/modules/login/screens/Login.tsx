af82d3729b3befc9f20489310e32c3dd0633800a
Bryan <mauriciobryan1998@gmail.com>
2024-02-24 14:55:38 -0300

    Creating Generic Button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,18 +1,22 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
+import Button from '../../../shared/components/Button';
 import Input from '../../../shared/components/Input';
 import { ContainerLogin } from '../styles/login.style';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('clicked');
+  };
   return (
     <View>
       <ContainerLogin>
-        <Text>Login</Text>
         <Input
           onChange={(e) => {
             console.log(e.nativeEvent.text);
           }}
         />
+        <Button title="Sign in" margin="16px" onPress={handleOnPress} />
       </ContainerLogin>
     </View>
   );
