a0a927c324a5f0d199fab207b7526109aa43b3e5
Bryan <mauriciobryan1998@gmail.com>
2024-04-20 15:19:30 -0300

    Inserting await function on Splash Screen

--- src/modules/home/screens/Home.tsx
+++ src/modules/home/screens/Home.tsx
@@ -1,6 +1,17 @@
+import { View } from 'react-native';
+import { useNavigation } from '../../../../react-navigation';
+import Button from '../../../shared/components/Button';
 import { Text } from '../../../shared/components/Text';
+import { logout } from '../../../shared/functions/connection/auth';
 
 const Home = () => {
-  return <Text>Home</Text>;
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Text>Home</Text>
+
+      <Button title="Sair" onPress={() => logout(navigation)} />
+    </View>
+  );
 };
 export default Home;
--- src/modules/splash/screens/Splash.tsx
+++ src/modules/splash/screens/Splash.tsx
@@ -1,40 +1,52 @@
 import { useEffect } from 'react';
-import { useRequest } from '../../login/hooks/useRequest'; 
+import { useRequest } from '../../login/hooks/useRequest';
 import { ContainerSplash, ImageLogoSplash } from '../styles/splash.style';
 import { useNavigation } from '../../../../react-navigation';
 import { useUserReducer } from '../../../store/reducers/userReducer/useUserReducer';
 import { URL_USER } from '../../../shared/constants/urls';
 import { MethodEnum } from '../../../enums/methods.enum';
 import { MenuUrl } from '../../../shared/enums/MenuUrl.enum';
+import { getAuthorizationToken } from '../../../shared/functions/connection/auth';
 
 const Splash = () => {
-    const { reset } = useNavigation();
-    const { request } = useRequest();
-    const { setUser } = useUserReducer();
-  
-    useEffect(() => {
-      const verifyLogin = async () => {
-        const returnUser = await request({
+  const { reset } = useNavigation();
+  const { request } = useRequest();
+  const { setUser } = useUserReducer();
+
+  useEffect(() => {
+    const findUser = async () => {
+      let returnUser = undefined;
+      const token = await getAuthorizationToken();
+      if (token) {
+        returnUser = await request({
           url: URL_USER,
           method: MethodEnum.GET,
           saveGlobal: setUser,
         });
-  
-        if (returnUser) {
-          reset({
-            index: 0,
-            routes: [{ name: MenuUrl.HOME }],
-          });
-        } else {
-          reset({
-            index: 0,
-            routes: [{ name: MenuUrl.LOGIN }],
-          });
-        }
-      };
-  
-      verifyLogin();
-    }, []);
+      }
+      return returnUser;
+    };
+
+    const verifyLogin = async () => {
+      const [returnUser] = await Promise.all([
+        findUser(),
+        new Promise<void>((r) => setTimeout(r, 5000)),
+      ]);
+      if (returnUser) {
+        reset({
+          index: 0,
+          routes: [{ name: MenuUrl.HOME }],
+        });
+      } else {
+        reset({
+          index: 0,
+          routes: [{ name: MenuUrl.LOGIN }],
+        });
+      }
+    };
+
+    verifyLogin();
+  }, []);
   return (
     <ContainerSplash>
       <ImageLogoSplash resizeMode="contain" source={require('../../../assets/images/logo.png')} />
