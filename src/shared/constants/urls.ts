d55a4486d820ee7c1f9bc4ee48c8eea0465c5439
Bryan <mauriciobryan1998@gmail.com>
2024-04-20 14:21:17 -0300

    Inserting token in axios request

---
+++ src/shared/constants/urls.ts
@@ -0,0 +1,2 @@
+export const URL_USER = 'http://172.21.112.1:8080/user'
+//127.0.0.1
--- src/shared/functions/connection/connectionAPI.ts
+++ src/shared/functions/connection/connectionAPI.ts
@@ -1,21 +1,27 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import { MethodEnum } from '../../../enums/methods.enum';
+import { getAuthorizationToken } from './auth';
 
 export type TMethodType = 'get' | 'post' | 'put' | 'patch' | 'delete';
 export default class ConnectionAPI {
   static async call<T>(url: string, method: TMethodType, body?: unknown): Promise<T> {
+    const token = await getAuthorizationToken()
+    const config: AxiosRequestConfig = {
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json'
+      },
+    };
     switch (method) {
       case MethodEnum.DELETE:
       case MethodEnum.GET:
-        return (await axios[method]<T>(url)).data;
+        return (await axios[method]<T>(url, config)).data;
       case MethodEnum.POST:
       case MethodEnum.PUT:
       case MethodEnum.PATCH:
-        return (await axios[method]<T>(url, body)).data;
-
       default:
-        return (await axios[method]<T>(url)).data;
+        return (await axios[method]<T>(url, body, config)).data;
     }
   }
 
